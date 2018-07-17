b39d1ea5c65e8747b7dd32ea5ab60ce7d6bd24fa
grandballoon <luke.sovern.conley@gmail.com>
2018-07-17 11:55:10 -0400

    puppies

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    @data = params
+    @puppy = Puppy.new(@data[:name], @data[:breed], @data[:age])
+    erb :display_puppy
+  end
+
+  get '/display_puppy' do
+    erb :display_puppy
+  end
 end
